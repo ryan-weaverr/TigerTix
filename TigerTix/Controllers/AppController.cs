9649b7a9846dde323a0b48cec9000fd2be99b729
ryan-weaverr <weaverryan575@gmail.com>
2022-03-31 11:07:54 -0400

    gg

--- TigerTix/Controllers/AppController.cs
+++ TigerTix/Controllers/AppController.cs
@@ -3,12 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TigerTix.Data;
 using TigerTix.ViewModels;
 
 namespace TigerTix.Controllers
 {
     public class AppController : Controller
     {
+        private readonly TigerTixContext _context;
+        public AppController(TigerTixContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ShowUsers()
+        {
+            var results = from u in _context.Users
+                          select u;
+            return View(results.ToList());
+        }
+
         public IActionResult Index(IndexViewModel model)
         {
             return View();
