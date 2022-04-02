787c293f7346ca6cc45ac1d860f9953404fb006e
ryan-weaverr <weaverryan575@gmail.com>
2022-04-01 20:01:56 -0400

    gg

--- TigerTix/Startup.cs
+++ TigerTix/Startup.cs
@@ -18,7 +18,7 @@ namespace TigerTix
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<TigerTixContext>();
+            
             services.AddControllersWithViews();
 
             services.AddDbContext<TigerTixContext>(cfg =>
@@ -27,12 +27,10 @@ namespace TigerTix
             });
         }
 
-        
-           
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseDefaultFiles();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
