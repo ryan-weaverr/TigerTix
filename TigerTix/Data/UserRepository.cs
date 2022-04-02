affb49fc7b4b68a841a7a1be1938f26f3c781604
ryan-weaverr <weaverryan575@gmail.com>
2022-04-02 18:31:37 -0400

    gg

---
+++ TigerTix/Data/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TigerTix.Data
+{
+    public class UserRepository
+    {
+        private readonly TigerTixContext _context;
+        public UserRepository(TigerTixContext context)
+        {
+            _context = context;
+        }
+
+    }
+
+}
